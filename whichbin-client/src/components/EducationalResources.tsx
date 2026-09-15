cf2b56b15bef815f4e0b0f14445358f9261c715f
Todd Uhl <uhlto13@gmail.com>
2026-09-14 21:04:07 -0400

    Add educational resources rough draft section

---
+++ whichbin-client/src/components/EducationalResources.tsx
@@ -0,0 +1,57 @@
+import './EducationalResources.css'
+
+function EducationalResources() {
+  const resources = [
+    {
+      icon: '🍃',
+      title: 'Recycling Basics',
+      description: 'The essentials of what can and cannot be recycled.',
+    },
+    {
+      icon: '⚙',
+      title: 'How to Prepare Items',
+      description: 'Simple steps to make your items recyclable.',
+    },
+    {
+      icon: '⚠',
+      title: 'Common Mistakes',
+      description: 'Avoid these common recycling mistakes.',
+    },
+    {
+      icon: '📄',
+      title: 'Orlando Recycling Guide',
+      description: 'Full details from the City of Orlando.',
+    },
+  ]
+
+  return (
+    <div className="resources-page">
+      <h1>Educational Resources</h1>
+
+      <p className="resources-intro">
+        Learn more about recycling and how to make a bigger impact.
+      </p>
+
+      <div className="resources-list">
+        {resources.map((resource) => (
+          <div className="resource-card" key={resource.title}>
+            <div className="resource-icon">
+              {resource.icon}
+            </div>
+
+            <div className="resource-content">
+              <h2>{resource.title}</h2>
+              <p>{resource.description}</p>
+            </div>
+
+            <div className="resource-arrow">
+              ›
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default EducationalResources
