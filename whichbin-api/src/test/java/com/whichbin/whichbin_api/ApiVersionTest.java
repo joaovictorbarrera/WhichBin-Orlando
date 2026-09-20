973a682d51a0337c8b99ccacdd7d5d603cf01d40
Clay VanZile <cvanzile@mail.valenciacollege.edu>
2026-09-20 14:39:11 -0400

    Added unit tests to verify API version.

---
+++ whichbin-api/src/test/java/com/whichbin/whichbin_api/ApiVersionTest.java
@@ -0,0 +1,28 @@
+package com.whichbin.whichbin_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+@AutoConfigureMockMvc
+class ApiVersionTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void apiShouldReturnVersion() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith("application/json"))
+                .andExpect(jsonPath("$.version").value("1.0"));
+    }
+}
