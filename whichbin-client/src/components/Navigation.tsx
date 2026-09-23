e8176323205185b4e3166fd1a29a4d7255d81925
Joao Barrera <joao.victor.lotfi@gmail.com>
2026-09-22 21:01:07 -0400

    added logo icon

--- whichbin-client/src/components/Navigation.tsx
+++ whichbin-client/src/components/Navigation.tsx
@@ -8,7 +8,6 @@ import {
   FiX,
   FiInfo,
   FiHelpCircle,
-  FiSettings,
   FiLock,
   FiActivity,
 } from 'react-icons/fi'
@@ -44,6 +43,7 @@ function Navigation() {
     <>
       <header className="top-navigation">
         <NavLink to="/" end className="app-title" onClick={closeMenu}>
+          <FaRecycle aria-hidden="true" />
           WhichBin Orlando
         </NavLink>
 
